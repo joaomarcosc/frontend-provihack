158d5fc607b2d3e71cd0ceb0ec922c359fab73cf
joaomarcos <j.marcos9@hotmail.com>
2022-04-28 00:37:55 -0300

    feat: create custom input to use in project

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,5 @@
 import styled from 'styled-components';
 
 export const Container = styled.button``;
+
+export const CustomInput = styled.input``;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface IInput {
+  icon?: ReactNode;
+}
+
+export const Container = styled.section`
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  width: fit-content;
+  border-radius: 12px;
+  padding: 8px 20px;
+  box-shadow: 0px 4px 15px rgba(13, 38, 1, 0.13);
+  border-radius: 12px;
+`;
+
+export const CustomInput = styled.input<IInput>`
+  border: none;
+  border-radius: 12px;
+  outline: none;
+  font-size: 16px;
+  font-weight: 500;
+  color: 000;
+
+  &::placeholder {
+    color: #999999;
+  }
+
+  ${(props) =>
+    props.icon &&
+    `
+    padding-left: 26px
+  `}
+`;
+
+export const Icon = styled.p`
+  font-size: 16px;
+  color: #999999;
+  margin: 0;
+  position: absolute;
+  top: 0;
+  transform: translateY(40%);
+`;
