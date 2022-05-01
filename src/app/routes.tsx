c9062c7de322d4e7d4590748b9471c92e7e0d962
Renan Lage <renanlopresti@gmail.com>
2022-05-01 18:15:33 -0300

    feat: create form suport

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -8,6 +8,7 @@ const HelpPage = React.lazy(() => import('../containers/HelpPage'));
 const InformationView = React.lazy(
   () => import('../containers/InformationView')
 );
+const SuportForm = React.lazy(() => import('../containers/SuportForm'));
 
 export function Routes() {
   return (
@@ -18,6 +19,7 @@ export function Routes() {
           <Route path="tipos-de-materiais" element={<RecycleTypes />} />
           <Route path="submeter-sugestao" element={<SuggestionForm />} />
           <Route path="informacao" element={<InformationView />} />
+          <Route path="suporte" element={<SuportForm />} />
         </Route>
         <Route path="/help">
           <Route index element={<HelpPage />} />
