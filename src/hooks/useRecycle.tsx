deb51913d7d9d8797b7eb301fdcec9720d6383a7
joaomarcos <j.marcos9@hotmail.com>
2022-05-01 11:40:17 -0300

    feat: create integrations and hooks on app

--- src/hooks/useRecycle.tsx
+++ src/hooks/useRecycle.tsx
@@ -1,8 +1,31 @@
-import { getRecycleTypes } from 'api/requests/recycle';
-import { useQuery } from 'react-query';
+import {
+  getCompanyByName,
+  getRecycleTypes,
+  postFormSuggestion,
+  postFormSupport
+} from 'api/requests/recycle';
+import { useMutation, useQuery } from 'react-query';
 
 export function useRecycle() {
   const query = useQuery('recycle', getRecycleTypes);
 
   return query;
+}
+
+export function useCompanyByName(name: string) {
+  const query = useQuery(['company', name], () => getCompanyByName(name));
+
+  return query;
+}
+
+export function useFormSuggestion() {
+  const mutation = useMutation(postFormSuggestion);
+
+  return mutation;
+}
+
+export function useFormSupport() {
+  const mutation = useMutation(postFormSupport);
+
+  return mutation;
 }
